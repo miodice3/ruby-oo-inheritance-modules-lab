9bb24c1afe3be6e4b7bcbd1afcce6749d6d2096b
Michael Iodice <miodice3@gmail.com>
2020-10-16 15:21:41 -0600

    Done.

---
+++ lib/concerns/findable.rb
@@ -0,0 +1,17 @@
+require 'pry'
+module Findable
+    
+    def find_by_name(name)
+        #binding.pry
+        all.detect{|a| a.name == name}
+#        self.all.detect{|a| a.name == name}
+
+        # def self.find_by_name(name)
+        #     @@songs.detect{|a| a.name == name}
+        # end
+
+        # def self.find_by_name(name)
+        #     @@artists.detect{|a| a.name == name}
+        # end
+    end
+end
